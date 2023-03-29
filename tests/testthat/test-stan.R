14aadec141bfbecd0b47a4cb3ec864d1d4642a58
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2023-03-29 16:24:32 +0100

    Refactor tests and ignore Stan diagnostics

--- tests/testthat/test-stan.R
+++ tests/testthat/test-stan.R
@@ -24,42 +24,34 @@ test_that("utils basic characteristics", {
   expect_length(util_code, 1)
 })
 
-test_that("basic individual model compiles", {
-  args = c(
-    infer_duration(
-      pa_model = pa_double_censor(c(1, 1), c(2, 2), c(3, 3), c(4, 4)),
-      pt_model = pt_individual(tS = matrix(rep(1, 8), nrow = 2, ncol = 4))
-    ),
-    iter = 5, chains = 1, refresh = 0, cores = 1, warmup = 2
-  )
-  result = do.call(rstan::stan, args)
+check_args_run = function(args) {
+  args_full = c(args, iter = 5, chains = 1, refresh = 0, cores = 1, warmup = 2)
+  result = suppressWarnings(do.call(rstan::stan, args_full))
   S = rstan::extract(result, "S[1]")[[1]]
   expect_length(S, 3)
+}
+
+test_that("basic individual model compiles", {
+  infer_duration(
+      pa_model = pa_double_censor(c(1, 1), c(2, 2), c(3, 3), c(4, 4)),
+      pt_model = pt_individual(tS = matrix(rep(1, 8), nrow = 2, ncol = 4))
+  ) |>
+    check_args_run()
 })
 
 test_that("basic total model compiles", {
-  args = c(
-    infer_duration(
+  infer_duration(
       pa_model = pa_double_censor(c(1, 1), c(2, 2), c(3, 3), c(4, 4)),
       pt_model = pt_total(tS = rep(1, 4), mu_n = 4, r_n = 1)
-    ),
-    iter = 5, chains = 1, refresh = 0, cores = 1, warmup = 2
-  )
-  result = do.call(rstan::stan, args)
-  S = rstan::extract(result, "S[1]")[[1]]
-  expect_length(S, 3)
+    ) |>
+      check_args_run()
 })
 
 test_that("inferring sensitivity compiles", {
-  args = c(
-    infer_duration(
+  infer_duration(
       pa_model = pa_double_censor(c(1, 1), c(2, 2), c(3, 3), c(4, 4)),
       pt_model = pt_individual(tS = matrix(rep(1, 8), nrow = 2, ncol = 4)),
       sensitivity_model = infer_sensitivity(n_pos = 2, n_intermittent_neg = 3)
-    ),
-    iter = 5, chains = 1, refresh = 0, cores = 1, warmup = 2
-  )
-  result = do.call(rstan::stan, args)
-  S = rstan::extract(result, "sensitivity")[[1]]
-  expect_length(S, 3)
+    ) |>
+      check_args_run()
 })
