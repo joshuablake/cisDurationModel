d7e5babac368bf18058b2dc5f0a46c79398d918b
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2023-04-05 15:01:04 +0100

    Rewrite matrix code to look multiple tests ahead, also more efficient now

--- R/form_matrices.R
+++ R/form_matrices.R
@@ -4,20 +4,44 @@
 #' @param times The times where infections are allowed to occur
 #' @param max_S The maximal value of S that is allowed
 #' @param min_times A vector of times where individual's were able to be infected from, ordered the same as test_time_list
+#' @param max_N Create matrices for up to N tests ahead
 #' @export
-form_tS_matrix = function(test_time_list, times, max_S, min_times = -Inf) {
+form_tS_matrices = function(test_time_list, times, max_S, min_times = -Inf, max_N = 1) {
   if (length(min_times) == 1) min_times = rep(min_times, length(test_time_list))
   stopifnot(length(min_times) == length(test_time_list))
-  t(mapply(test_times = test_time_list, min_time = min_times, FUN = create_td_row,
-           MoreArgs = list(all_times = times, max_S = max_S))) / length(times)
+  counts = purrr::map2(
+    test_time_list, min_times,
+    ~create_tdNs_it(.x, times, max_S, .y, max_N) / length(times)
+  )
+  combined_matrix = do.call(rbind, counts)
+  vector_to_matrix = function(x) {
+    matrix(x, nrow = length(test_time_list), ncol = max_S, byrow = TRUE)
+  }
+  return(
+    purrr::map(1:max_N, ~vector_to_matrix(combined_matrix[,.x]))
+  )
 }
 
-create_td_row = function(test_times, all_times, max_S, min_time) {
+create_tdNs_it = function(test_times, all_times, max_S, min_time, max_N) {
   all_times = all_times[all_times >= min_time]
-  tN_it = purrr::map_dbl(
-    all_times,
-    ~min(c(test_times[test_times >= .x], Inf)) - .x
-  )
-  tS_i = purrr::map_dbl(1:max_S, ~sum(tN_it + 1 == .x))
-  return(tS_i)
+  sorted_test_times = c(sort(test_times), rep(Inf, max_N))
+  tNs_it = do.call(
+    rbind,
+    purrr::map(
+      all_times,
+      ~(sorted_test_times[sorted_test_times >= .x])[1:max_N] - .x
+    )
+  ) |>
+    magrittr::set_colnames(1:max_N)
+  # Count each unique tN
+  result = do.call(
+    cbind,
+    purrr::map(
+      1:max_N,
+      ~factor(tNs_it[,.x] + 1, levels = 1:max_S) |>
+        table()
+    )
+  ) |>
+    matrix(ncol = max_N)
+  return(result)
 }
--- tests/testthat/test-form_matrices.R
+++ tests/testthat/test-form_matrices.R
@@ -12,7 +12,7 @@ run_simple_case = function(n_indiv, max_S) {
   expected_result = matrix(rep(c(3, 3, 2, rep(0, max_S - 3)), n_indiv),
                            nrow = n_indiv, byrow = TRUE) / 11
   # stopifnot(sum(expected_result) == length(tN_it) - sum(tN_it == Inf))
-  result = form_tS_matrix(test_time_list, 0:10, max_S)
+  result = form_tS_matrices(test_time_list, 0:10, max_S)[[1]]
   expect_equal(dim(result), c(n_indiv, max_S))
   expect_equal(result, expected_result)
 }
@@ -26,7 +26,7 @@ run_simple_case2 = function(n_indiv, max_S) {
   expected_result = matrix(rep(c(3, 2, 2, 1, rep(0, max_S - 4)), n_indiv),
                            nrow = n_indiv, byrow = TRUE) / 11
   # stopifnot(sum(expected_result) == length(tN_it) - sum(tN_it == Inf))
-  result = form_tS_matrix(test_time_list, 0:10, max_S)
+  result = form_tS_matrices(test_time_list, 0:10, max_S)[[1]]
   expect_equal(dim(result), c(n_indiv, max_S))
   expect_equal(result, expected_result)
 }
@@ -51,7 +51,7 @@ test_that("two different", {
       3, 2, 2, 1, rep(0, max_S - 4)),
     nrow = 2, byrow = TRUE
   ) / 11
-  result = form_tS_matrix(test_time_list, 0:10, max_S)
+  result = form_tS_matrices(test_time_list, 0:10, max_S)[[1]]
   expect_equal(dim(result), c(2, max_S))
   expect_equal(result, expected_result)
 })
@@ -69,7 +69,83 @@ test_that("simple case with min times", {
                              0, 0, 0, 0, 0),
                            nrow = n_indiv, byrow = TRUE) / 11
   # stopifnot(sum(expected_result) == length(tN_it) - sum(tN_it == Inf))
-  result = form_tS_matrix(test_time_list, 0:10, max_S, min_times = c(-1, 3, 8))
+  result = form_tS_matrices(test_time_list, 0:10, max_S, min_times = c(-1, 3, 8))[[1]]
+  expect_equal(dim(result), c(n_indiv, max_S))
+  expect_equal(result, expected_result)
+})
+
+run_simple_cases_N2 = function(n_indiv, max_S) {
+  run_simple_case_N2(n_indiv, max_S)
+  run_simple_case2_N2(n_indiv, max_S)
+}
+
+run_simple_case_N2 = function(n_indiv, max_S) {
+  test_times = c(1, 4, 7)
+  test_time_list = rep(list(test_times), n_indiv)
+  # times = 0:10
+  # tN_it = c(4:3, 5:3, rep(Inf, 6))
+  # stopifnot(length(tN_it) == length(times))
+  expected_result = matrix(rep(c(0, 0, 0, 2, 2, 1, rep(0, max_S - 6)), n_indiv),
+                           nrow = n_indiv, byrow = TRUE) / 11
+  # stopifnot(sum(expected_result) == length(tN_it) - sum(tN_it == Inf))
+  result = form_tS_matrices(test_time_list, 0:10, max_S, max_N = 2)[[2]]
+  expect_equal(dim(result), c(n_indiv, max_S))
+  expect_equal(result, expected_result)
+}
+
+run_simple_case2_N2 = function(n_indiv, max_S) {
+  test_times = c(2, 6, 7)
+  test_time_list = rep(list(test_times), n_indiv)
+  # times = 0:10
+  # tN_it = c(6:4, 4:1, rep(Inf, 4))
+  # stopifnot(length(tN_it) == length(times))
+  expected_result = matrix(rep(c(0, 1, 1, 1, 2, 1, 1, rep(0, max_S - 7)), n_indiv),
+                           nrow = n_indiv, byrow = TRUE) / 11
+  # stopifnot(sum(expected_result) == length(tN_it) - sum(tN_it == Inf))
+  result = form_tS_matrices(test_time_list, 0:10, max_S, max_N = 2)[[2]]
+  expect_equal(dim(result), c(n_indiv, max_S))
+  expect_equal(result, expected_result)
+}
+
+test_that("simple case, N=2", {
+  run_simple_cases_N2(1, 8)
+})
+
+test_that("two identical, N=2", {
+  run_simple_cases_N2(2, 8)
+})
+
+test_that("large max S, N=2", {
+  run_simple_cases_N2(1, 100)
+})
+
+test_that("two different, N=2", {
+  max_S = 100
+  test_time_list = list(c(1, 4, 7), c(2, 6, 7))
+  expected_result = matrix(
+    c(0, 0, 0, 2, 2, 1, rep(0, max_S - 6),
+      0, 1, 1, 1, 2, 1, 1, rep(0, max_S - 7)),
+    nrow = 2, byrow = TRUE
+  ) / 11
+  result = form_tS_matrices(test_time_list, 0:10, max_S, max_N = 2)[[2]]
+  expect_equal(dim(result), c(2, max_S))
+  expect_equal(result, expected_result)
+})
+
+test_that("simple case with min times, N=2", {
+  n_indiv = 3
+  max_S = 6
+  test_times = c(1, 4, 7)
+  test_time_list = rep(list(test_times), n_indiv)
+  # times = 0:10
+  # tN_it = c(6:4, 4:1, rep(Inf, 4))
+  # stopifnot(length(tN_it) == length(times))
+  expected_result = matrix(c(0, 0, 0, 2, 2, 1,
+                             0, 0, 0, 1, 1, 0,
+                             0, 0, 0, 0, 0, 0),
+                           nrow = n_indiv, byrow = TRUE) / 11
+  # stopifnot(sum(expected_result) == length(tN_it) - sum(tN_it == Inf))
+  result = form_tS_matrices(test_time_list, 0:10, max_S, min_times = c(-1, 3, 8), max_N = 2)[[2]]
   expect_equal(dim(result), c(n_indiv, max_S))
   expect_equal(result, expected_result)
 })
