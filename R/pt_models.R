ddedfdd1fada431ce198818467f093fcde34c784
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2023-04-26 11:37:36 +0100

    False negatives with info on 2nd negs for total model

--- R/pt_models.R
+++ R/pt_models.R
@@ -1,6 +1,6 @@
 #' Likelihood contribution for the individual model for p_t
 #'
-#' @param tS Matrix of size (n_a x max_S) where element (i, j) is the contribution of S_j for individual i
+#' @param tS_matrices Matrix/ces of size (n_a x max_S) as returned by form_tS_matrices
 #' @param mu_ni Prior mean for the negative binomial prior on n_i
 #' @param r_ni Prior size for the negative binomial prior on n_i. Set to zero for an improper prior proportion to 1/n_i
 #' @export
@@ -18,18 +18,42 @@ pt_individual = function(tS, mu_ni = 1, r_ni = 0) {
 
 #' Likelihood contribution for the total model for p_t
 #'
-#' @param tS Vector of size max_S where element i is the contribution of S_i
+#' @param tS_matrices Matrix/ces of size (n_a x max_S) as returned by form_tS_matrices
 #' @param mu_n Prior mean for the negative binomial prior on n.
 #' @param r_n Prior size for the negative binomial prior on n.
 #' @export
-pt_total = function(tS, mu_n = 1, r_n = 0) {
-  list(
-    data_args = list(tS = tS),
-    data_code = list(
-      data = list("row_vector[max_S] tS;")
-    ),
-    model = list(
-      glue::glue("target += -lmultiply({r_n} + n_a, (sensitivity * {mu_n} * tS * S + {r_n}));")
+pt_total = function(tS_matrices, mu_n = 1, r_n = 0) {
+  stopifnot(length(tS_matrices) <= 2)
+  if (length(tS_matrices) == 1) {
+    result = list(
+      data_args = list(tS = colMeans(tS_matrices[[1]])),
+      data_code = list(
+        data = list("row_vector[max_S] tS;")
+      ),
+      transformed_parameters_code = list(
+        "one_minus_pt = sensitivity * tS1 * S;"
+      )
     )
+  } else {
+    result = list(
+      data_args = list(
+        tS1 = colMeans(tS_matrices[[1]])
+        tS2 = colMeans(tS_matrices[[2]])
+      ),
+      data_code = list(
+        data = list(
+          "row_vector[max_S] tS1;"
+          "row_vector[max_S] tS2;"
+        )
+      ),
+      transformed_parameters_code = list(
+        "one_minus_pt = sensitivity * tS1 * S + (1 - sensitivity) * tS2 * S;"
+      )
+    )
+  }
+  result$transformed_parameters_declare = list("real one_minus_pt;")
+  result$model = list(
+    glue::glue("target += -lmultiply({r_n} + n_a, {r_n} + {mu_n} * one_minus_pt);")
   )
+  return(result)
 }
